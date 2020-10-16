00cdfc0e8446fe763e9cf53bc010335264e704f6
Manpreet Rathore <43753613+manpreetrathore@users.noreply.github.com>
2020-10-16 22:50:49 +0530

    Revert "Total redesign"

--- portfolio-website/js/master.js
+++
@@ -1,5 +0,0 @@
-// SCROLLREVEAL //
-ScrollReveal().reveal('#intro', { delay: 100 });
-ScrollReveal().reveal('#twitter', { delay: 100 });
-ScrollReveal().reveal('#timeline', { delay: 100 });
-ScrollReveal().reveal('#contact', { delay: 100 });
